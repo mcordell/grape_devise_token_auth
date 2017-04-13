fea0e8cc94dce3265ea837fa6a98b87dd058f621
bartes <bartesrlz@gmail.com>
2017-04-13 17:43:48 +0200

    merge headers from batch requests

--- lib/grape_devise_token_auth/auth_headers.rb
+++ lib/grape_devise_token_auth/auth_headers.rb
@@ -25,25 +25,29 @@ module GrapeDeviseTokenAuth
     end
 
     def within_batch_request_window?
-      end_of_window = Time.parse(resource.tokens[client_id]['updated_at']) +
+      end_of_window = Time.parse(resource.tokens[client_id]['updated_at'].to_s) +
                       GrapeDeviseTokenAuth.batch_request_buffer_throttle
 
       request_start < end_of_window
     end
 
     def auth_headers_from_resource
-      auth_headers = {}
-      resource.with_lock do
-        if !GrapeDeviseTokenAuth.change_headers_on_each_request
-          auth_headers = resource.extend_batch_buffer(token, client_id)
-        elsif batch_request?
-          resource.extend_batch_buffer(token, client_id)
-          # don't set any headers in a batch request
-        else
-          auth_headers = resource.create_new_auth_token(client_id)
+      if !GrapeDeviseTokenAuth.change_headers_on_each_request
+        return {} if resource.reload.tokens[client_id].nil?
+        resource.build_auth_header(token, client_id)
+      else
+        resource.with_lock do
+          return {} if resource.tokens[client_id].nil?
+          if batch_request?
+            auth_headers = resource.extend_batch_buffer(token, client_id)
+            auth_headers[DeviseTokenAuth.headers_names[:"access-token"]] = ' '
+            auth_headers[DeviseTokenAuth.headers_names[:"expiry"]] = ' '
+            auth_headers
+          else
+            resource.create_new_auth_token(client_id)
+          end
         end
       end
-      auth_headers
     end
   end
 end
